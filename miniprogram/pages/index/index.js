4986fd560816a3944a430a3f9806c331c4a75eae
wyc <844544239@qq.com>
2019-12-16 15:35:57 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    swiper : {
+      imgUrl: [ 
+                'cloud://test-oehro.7465-test-oehro-1300850972/img/silder5.jpg', 
+                'cloud://test-oehro.7465-test-oehro-1300850972/img/silder3.jpg',    
+                'cloud://test-oehro.7465-test-oehro-1300850972/img/silder4.jpg',  
+              ],
+    }
+  },
+  onLoad(){
+    console.log('123');
+  }
+})
